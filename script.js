fc6b935efbbd9237d6a35ba32cbaa1520b204561
KULSUM <you@example.com>
2022-08-11 06:24:44 +0600

    second task commit

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// Fist task
 function radianToDegree(radian){
   return radian * (180 / Math.PI)
 }
 let getResult = radianToDegree(25)
-console.log( getResult.toFixed(2) )
+console.log( getResult.toFixed(2), '\n')
+
+// Second task
+// let fileName = 'app.js';
+function JavaScriptFile(String){  
+    if(String === 'index.js'){
+        return true
+    }else{
+        return false
+    }
+}
+let getJavaScriprFile = JavaScriptFile('index.js')
+console.log(getJavaScriprFile, '\n')
+
