ad04aa37a7499a6efd38bdccdffbdaf9bfd2db57
KULSUM <you@example.com>
2022-08-11 06:26:56 +0600

    third task commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function radianToDegree(radian){
   return radian * (180 / Math.PI)
 }
 let getResult = radianToDegree(25)
+
 console.log( getResult.toFixed(2), '\n')
 
 // Second task
@@ -16,4 +17,21 @@ function JavaScriptFile(String){
 }
 let getJavaScriprFile = JavaScriptFile('index.js')
 console.log(getJavaScriprFile, '\n')
+
+// Third task
+
+function oilPrice(diesel, petrol, octen ){
+    const dieselPricePerLiter = 114;
+    const petrolPricePerLiter = 130;
+    const octenPricePerLiter = 135;
+
+    const dieselPrice = dieselPricePerLiter * diesel;
+    const petrolPrice = petrolPricePerLiter * petrol;
+    const octenPrice = octenPricePerLiter * octen;
+    const totalOilCost = dieselPrice + petrolPrice + octenPrice;
+    return totalOilCost;
+
+}
+const showOilCost = oilPrice(5,3,4)
+console.log(showOilCost , '\n')
 
